fb4c7252bbe6b6f8805d1a1d1fda251bb18577fc
rajroj2014 <rajroj2014@users.noreply.github.com>
2021-04-19 11:57:01 +0300

    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 # app.config['SECRET_KEY'] = 'ssfSDFSDFSFSFSFsFsFsFsDFSDFsDFSFSfD'
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
